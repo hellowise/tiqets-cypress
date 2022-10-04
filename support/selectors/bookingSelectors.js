13bd59881f6ebf5ba5a0b2e53d376d2f2e89a753
Héloïse <77694447+hellowise@users.noreply.github.com>
2022-10-04 16:55:28 +0200

    Add files via upload

---
+++ support/selectors/bookingSelectors.js
@@ -0,0 +1,14 @@
+
+export default {
+  handPickedOptionBookNow: 'div.BookWithPrice > div a',
+  selectDateFirstAttraction: 'div.CheckoutCommon__main div:nth-child(2) > div:nth-child(1) > button',
+  selectDateSecondAttraction: 'div.CheckoutCommon__main div:nth-child(3) > div:nth-child(1) > button',
+  selectTimeslotFirstAttraction: 'div:nth-child(2) > div:nth-child(1) > div.TimeslotSelect select',
+  selectTimeslotSecondAttraction: 'div:nth-child(2) > div:nth-child(1) > div.TimeslotSelect select',
+  validDates: 'button[name=day]:enabled',
+  selectTicketsFirstAttraction: 'div:nth-child(1) > div:nth-child(5) > button',
+  selectTicketsSecondAttraction: 'div:nth-child(1) > div:nth-child(4) > button',
+  ticketAddRemoveForAdult: 'div.TicketPicker:nth-of-type(2) button.QuantitySelector__button',
+  ticketAddRemoveForChild: 'div.TicketPicker:nth-of-type(4) button.QuantitySelector__button',
+  ticketAddRemoveForChildNoticeText: 'div.TicketPicker:nth-of-type(4) li'
+};
---
+++ support/selectors/homeSelectors.js
@@ -0,0 +1,9 @@
+
+export default {
+  handPickedSection: 'section[data-track-category="City Page Bundle Card"]',
+  handPickedBundleOptions: 'div.BundleCardsSlider article.Card',
+  combinationStatueAndEmpireState: 'div.CardInfoWrap a[href*="b1195"]',
+  languageAndCurrencySettings: 'button[data-track-category="Settings"]',
+  languageCurrencyOptions: 'select.SelectTag',
+  saveChanges: 'button[type="submit"]'
+};
