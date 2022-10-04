210b537941dc3bc69a1c41d39e9fb7b5d0633cce
Héloïse <77694447+hellowise@users.noreply.github.com>
2022-10-04 16:56:21 +0200

    Update bookingSelectors.js

--- cypress/support/selectors/bookingSelectors.js
+++ cypress/support/selectors/bookingSelectors.js
@@ -5,7 +5,7 @@ export default {
   selectDateSecondAttraction: 'div.CheckoutCommon__main div:nth-child(3) > div:nth-child(1) > button',
   selectTimeslotFirstAttraction: 'div:nth-child(2) > div:nth-child(1) > div.TimeslotSelect select',
   selectTimeslotSecondAttraction: 'div:nth-child(2) > div:nth-child(1) > div.TimeslotSelect select',
-  validDates: 'div.DayPicker-Day[aria-disabled=false]',
+  validDates: 'button[name=day]:enabled',
   selectTicketsFirstAttraction: 'div:nth-child(1) > div:nth-child(5) > button',
   selectTicketsSecondAttraction: 'div:nth-child(1) > div:nth-child(4) > button',
   ticketAddRemoveForAdult: 'div.TicketPicker:nth-of-type(2) button.QuantitySelector__button',
